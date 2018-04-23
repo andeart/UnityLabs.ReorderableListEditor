8059fa50db0ad21c04a6642ef8729c42fe33b9a3
Anurag Devanapally <anurag.devanapally@gmail.com>
2018-04-22 18:40:00 -0800

    Project split into VS project and Unity test project. Editor functionality is now in a DLL.

---
+++ TestProject/UnityReorderableListEditor/Assets/Scripts/MainContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+
+
+namespace UnityReorderableListEditor.V1.Context
+{
+
+    [Serializable]
+    public struct MyData
+    {
+        [SerializeField]
+        private string _name;
+        [SerializeField] [Range (0f, 1f)]
+        private float _floatInRange;
+    }
+
+
+    public class MainContext : MonoBehaviour
+    {
+        [SerializeField]
+        private int[] _integers;
+
+        [SerializeField]
+        private MyData[] _dataObjects;
+
+        [SerializeField]
+        private ScriptableObject[] _scriptableObjects;
+    }
+
+}
