eb863f5240dfd034564e19f85f56ad1471c45ead
Anurag D <andymations.ad@gmail.com>
2019-03-09 00:43:39 -0800

    Update andeart.pytools

--- tools/plainexist.py
+++ tools/plainexist.py
@@ -21,35 +21,36 @@ class PlainExist:
         args = parser.parse_args()
         self.__filepaths = args.files
         self.__dirpaths = args.dirs
-
-        self.__logger.log(f"Unparsed file paths: {self.__filepaths}"
-                            + f"\nUnparsed directory paths: {self.__dirpaths}",
-                            LogLevel.WARNING)
                 
         if (self.__filepaths == None and self.__dirpaths == None): self.__exit_with_error(1, "Neither file nor directory paths were provided for check.", parser.format_help())
-            
-        self.__filepaths = self.__filepaths.split(";")
-        self.__dirpaths = self.__dirpaths.split(";")
+        
+        if (self.__filepaths != None): self.__filepaths = self.__filepaths.split(";")
+        if (self.__dirpaths != None): self.__dirpaths = self.__dirpaths.split(";")
         self.__logger.log(f"Parsed file paths: {self.__filepaths}"
                             + f"\nParsed directory paths: {self.__dirpaths}",
                             LogLevel.WARNING)
 
 
-    def check(self):
+    def verify(self):
+        self.__verify_files()
+        self.__verify_dirs()
+        self.__logger.log("All files and directories were successfully verified.", LogLevel.SUCCESS)
+
+    def __verify_files(self):
+        if (self.__filepaths == None): return
         for filepath in self.__filepaths:
             if self.__is_file(filepath):
                 self.__logger.log(f"File was successfully found at: {filepath}")
             else:
                 self.__exit_with_error(1, f"File was not successfully found at: {filepath}")
 
+    def __verify_dirs(self):
+        if (self.__dirpaths == None): return
         for dirpath in self.__dirpaths:
             if self.__is_file(dirpath):
                 self.__logger.log(f"Directory was successfully found at: {dirpath}")
             else:
                 self.__exit_with_error(1, f"Directory was not successfully found at: {dirpath}")
-
-        self.__logger.log("All files and directories were successfully verified.", LogLevel.SUCCESS)
-
 
     def __exit_with_error(self, error_code, error_msg, usage_info = None):
         self.__logger.log("ERROR! Exiting..."
@@ -71,4 +72,4 @@ class PlainExist:
 
 if __name__ == "__main__":
     check = PlainExist(False)
-    check.check()
+    check.verify()
